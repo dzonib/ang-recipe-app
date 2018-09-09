6b05c083f2533390215f2a110dc5182553ff36e2
dzonib <nikolabosnjak381@gmail.com>
2018-09-09 11:06:50 +0200

    added custom route handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  nav = 'recipe';
+
+  handleNav(value: string): void {
+    this.nav = value;
+  }
 }
